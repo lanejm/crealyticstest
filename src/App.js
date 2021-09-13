01d776f1ef7e62953bd2e4279092405e4681cfaf
lanejm <67156874+lanejm@users.noreply.github.com>
2021-09-13 17:43:27 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+// import { getItems } from "../../itemModel";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [items, setItems] = useState(false);
+  useEffect(() => {
+    getProducts();
+  }, []);
+  function getProducts() {
+    fetch("http://localhost:3000")
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        setItems(data);
+      });
+  }
 }
-
 export default App;
